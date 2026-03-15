ae0569eed045e9e99e30e99b493c79c7ca7ad666
RakshitVaru <rakshitvaru97@gmail.com>
2026-03-15 16:04:58 -0400

    Delete Week_1 directory

--- Week_1/transformer_sample.py
+++
@@ -1,9 +0,0 @@
-# pip install transformers
-from  transformers import AutoTokenizer
-
-tok = AutoTokenizer.from_pretrained("bert-base-uncased")
-
-for word in ["Unbelievable", "the cat sat", "2024"]:
-    tokens = tok.tokenize(word)
-    ids    = tok.encode(word, add_special_tokens=False)
-    print(f"{word} → tokens: {tokens} | ids: {ids}")
